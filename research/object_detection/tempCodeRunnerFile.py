223659f1d3356955f959f6d47a713ffd143808c6
BRICE JOSHY <bricejoshy99@gmail.com>
2023-05-30 12:38:13 +0530

    model run issue

--- research/object_detection/tempCodeRunnerFile.py
+++ research/object_detection/tempCodeRunnerFile.py
@@ -1,7 +1,1 @@
-
-    category_index = label_map_util.create_category_index_from_labelmap(args.labelmap, use_display_name=True)
-
-    run_inference(detection_model, category_index, args.image_path)
-
-# Command to start script
-#  python .\detect_from_images.py -m ssd_mobilenet_v2_320x320_coco17_tpu-8\saved_model -l .\data\mscoco_label_map.pbtxt -i .\test_images
+python .\detect_from_images.py -m ssd_mobilenet_v2_320x320_coco17_tpu-8\saved_model -l .\data\mscoco_label_map.pbtxt -i .\test_images
